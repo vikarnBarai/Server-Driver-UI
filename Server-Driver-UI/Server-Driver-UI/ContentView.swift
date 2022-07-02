b68cf9234dfc383e0ca6785aec2830831e04fc25
vikarnBarai <45677528+vikarnBarai@users.noreply.github.com>
2022-07-02 19:14:53 +0530

    vikarn.barai@gmail.com
    initial comiit

---
+++ Server-Driver-UI/Server-Driver-UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Server-Driver-UI
+//
+//  Created by Vikarn Barai on 02/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Server-Driver-UI/Server-Driver-UI/Server_Driver_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Server_Driver_UIApp.swift
+//  Server-Driver-UI
+//
+//  Created by Vikarn Barai on 02/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Server_Driver_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
